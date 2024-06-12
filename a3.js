12c2c992430230f0462a52b332ed178d898e2c9c
Jaeha <jaeha0208@gmail.com>
2024-06-13 01:30:56 +1000

    613 1am

--- a3.js
+++ a3.js
@@ -1,1 +1,7 @@
-console/log("hello, world");
+const bar =document.getElementsByClassName('mobile')
+const nav =document.getElementsByClassName('menu')
+if (bar){
+    bar.addEventListner('click'()=>{
+        nav.classlist.add('active')
+    })
+}
